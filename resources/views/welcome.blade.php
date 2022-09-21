a4cebeb87b064778d2fc468942a312efba0680a3
pergeld <pergeldaniel@gmail.com>
2022-09-21 20:08:37 +0200

    Install tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        @vite(['resources/css/app.css'])
+        @vite('resources/css/app.css')
     </head>
     <body class="antialiased">
         <div id="app"></div>
