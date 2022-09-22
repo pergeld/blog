799bd8d84ea054689a301cfd0d54ef8a3b932727
pergeld <pergeldaniel@gmail.com>
2022-09-22 10:14:54 +0200

    Show is_visible in article list

--- resources/views/admin/article/list.blade.php
+++ resources/views/admin/article/list.blade.php
@@ -54,7 +54,21 @@
                                 {{ optional($article->user)->name }}
                             </td>
                             <td class="hidden px-3 py-4 text-sm text-gray-500 md:table-cell">
-                                Látható?
+                                @if ($article->is_visible)
+                                    <div class="flex text-green-500">
+                                        <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                        Látható
+                                    </div>
+                                @else
+                                    <div class="flex text-red-500">
+                                        <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                        Nem látható
+                                    </div>
+                                @endif
                             </td>
                             <td class="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                                 <a href="#" class="inline-flex items-center justify-center rounded-md bg-gray-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-none sm:w-auto">
