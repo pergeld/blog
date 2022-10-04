9e1ed14e793c083b87adde9dca255ec997530370
pergeld <pergeldaniel@gmail.com>
2022-10-04 22:22:46 +0200

    Localization - Validation messages

---
+++ lang/hu/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'A jelszó legalább 6 karakteres legyen, és a két jelszó egyezzen meg.',
+    'reset' => 'A jelszó sikeresen visszaállítva!',
+    'sent' => 'Elküldtük e-mailben a jelszó visszaállításához szükséges linket!',
+    'token' => 'A jelszó-visszaállító link érvénytelen.',
+    'user' => "Ilyen e-mail címmel nincs regisztrált felhasználó.",
+
+];
