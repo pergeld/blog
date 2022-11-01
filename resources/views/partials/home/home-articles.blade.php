64ea60b697dfb5fa259bbc98b6500e683aaffc1a
pergeld <pergeldaniel@gmail.com>
2022-11-01 12:08:53 +0100

    Delete nav dots

--- resources/views/partials/home/home-articles.blade.php
+++ resources/views/partials/home/home-articles.blade.php
@@ -1,6 +1,6 @@
 <div class="container mt-64 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
     <section class="w-full">
-        <h2 id="articles" class="secondary-title">Hírek</h2>
+        <h2 id="articles" class="secondary-title">Bejegyzések</h2>
 
         <div class="space-y-12 sm:space-y-16 my-16 bg-white rounded sm:rounded-xl p-8 sm:p-16">
             @foreach ($articles as $article)
