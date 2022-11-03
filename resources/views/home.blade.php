31e273edf47dcbe0d9d01e8eb9a3ac45ff527c46
pergeld <pergeldaniel@gmail.com>
2022-11-03 20:22:45 +0100

    Home page - article link section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 <x-front-layout>
     @include('partials.home.banner')
+    @include('partials.home.home-article-link')
     @includeWhen($articleCategories->isNotEmpty(), 'partials.home.home-articlecategory')
     @includeWhen($articles->isNotEmpty(), 'partials.home.home-articles')
 </x-front-layout>
---
+++ resources/views/partials/home/home-article-link.blade.php
@@ -0,0 +1,31 @@
+<div id="home-article-link" class="overflow-hidden bg-gray-900 mt-32">
+    <div class="py-16 sm:px-2 lg:relative lg:py-20 lg:px-0">
+        <div class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 px-4 lg:max-w-7xl lg:grid-cols-2 lg:px-8 xl:gap-x-16 xl:px-12">
+            <div class="relative z-10 md:text-center lg:text-left">
+                <div class="relative">
+                    <p class="inline bg-gradient-to-r from-indigo-200 via-indigo-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+                        Nézd meg a kódot
+                    </p>
+                    <p class="mt-3 text-xl tracking-tight text-slate-400">
+                        A bejegyzésekben mindig megtalálod azokat a kódokat, melyeket az aktuális feladatot során írtam. <br>
+                        Másold le te is a kódokat és csináld velem együtt a feladatokat.
+                    </p>
+
+                    <div class="mt-8 flex gap-4 md:justify-center lg:justify-start">
+                        <a href="#" class="group inline-flex items-center justify-center rounded-full py-4 px-6 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-300 text-gray-700 hover:bg-blue-200 active:bg-blue-200 focus-visible:outline-blue-200">
+                            Irány a Blog
+                        </a>
+
+                        <a href="#" class="group inline-flex items-center justify-center rounded-full py-4 px-6 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-gray-700 text-gray-200 hover:bg-gray-500 active:bg-gray-500 focus-visible:outline-gray-500">
+                            GitHub
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="relative lg:static xl:pl-10">
+
+            </div>
+        </div>
+    </div>
+</div>
